45a3152b837f47fa80696a63a18065c3e244c918
therealkevinli <kevin.m.li888@gmail.com>
2018-11-13 20:43:28 -0800

    Update artist-page.component.ts
    this one should work

--- client/src/app/pages/artist-page/artist-page.component.ts
+++ client/src/app/pages/artist-page/artist-page.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { ArtistData } from '../../data/artist-data';
 import { TrackData } from '../../data/track-data';
 import { AlbumData } from '../../data/album-data';
-
+import { SpotifyService } from '../../services/spotify.service';//am I supposed to add this?
 @Component({
   selector: 'app-artist-page',
   templateUrl: './artist-page.component.html',
@@ -16,11 +16,38 @@ export class ArtistPageComponent implements OnInit {
 	topTracks:TrackData[];
 	albums:AlbumData[];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private spotifyService: SpotifyService) { }
 
   ngOnInit() {
   	this.artistId = this.route.snapshot.paramMap.get('id');
-    //TODO: Inject the spotifyService and use it to get the artist data, related artists, top tracks for the artist, and the artist's albums
+    //TODO: Inject the spotifyService and use it to get the artist 
+    //data, related artists, top tracks for the artist, 
+    //and the artist's albums
+
+    this.spotifyService.getArtist(this.artistId).then(
+      (res)=>{
+        this.artist = res;
+      }
+    );
+
+    this.spotifyService.getRelatedArtists(this.artistId).then(
+      (res) => {
+        this.relatedArtists = res;
+      }
+    );
+
+    this.spotifyService.getTopTracksForArtist(this.artistId).then(
+      (reslist)=>{
+        this.topTracks = reslist;
+      }
+    );
+
+    this.spotifyService.getAlbumsForArtist(this.artistId).then(
+      (reslist)=>{
+        this.albums = reslist;
+      }
+    );
+
   }
 
 }
